97461b222da2b5abf61e9363dc147b7d208c494f
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-08-03 14:43:39 +0200

    current.sh: Rework scripts
    No need for all the mumbo jumbo

--- tools/current.sh
+++ tools/current.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 DB_PATH="$1"
-DATA_PATH="$2"
-PERIOD="$3"
-
-echo "Usage: <DB_PATH> <DATA_PATH> <PERDIOD>"
-echo "Period available: hour, day, week, month, year"
 
 [ -z "$DB_PATH" ] && DB_PATH=/var/lib/tesla.rrd
-[ -z "$DATA_PATH" ] && DATA_PATH=$(pwd)/data.xml
 
-rrdtool xport --start now-1hour -e now \
-DEF:energy=${DB_PATH}:energy:LAST \
-XPORT:energy:"Energy" > ${DATA_PATH}
-
+rrdtool info ${DB_PATH} | grep last_ds | cut -d'"' -f2
