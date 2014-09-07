5619928aeb6a3234664e48bbdd3372e16f2c5f16
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-09-07 15:02:09 +0200

    Fix scripts regex

--- tools/fix_update_time.sh
+++ tools/fix_update_time.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rrdtool dump "$1" | sed -e "s/<lastupdate>1[0-9]\{9\}/<lastupdate>$2/" | rrdtool restore -f - "$1"
+rrdtool dump "$1" | sed -e "s/<lastupdate>.*/<lastupdate>$2<\/lastupdate>/" | rrdtool restore -f - "$1"
