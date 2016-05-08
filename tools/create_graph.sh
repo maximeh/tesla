82c70365370c1210288668849e319e45ab82679c
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2016-05-08 16:30:05 +0200

    create_graph: Indent

--- tools/create_graph.sh
+++ tools/create_graph.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+exec 1>/dev/null
+exec 2>&1
+
 PERIOD="$1"
 DB_PATH="$2"
 GRAPH_PATH="$3"
@@ -9,35 +12,32 @@ echo "Period available: hour, day, week, month, year"
 
 [ -z "$PERIOD" ] && PERIOD="hour"
 [ -z "$DB_PATH" ] && DB_PATH=/var/lib/tesla.rrd
-[ -z "$GRAPH_PATH" ] && GRAPH_PATH=$(pwd)/graph_${PERIOD}.png
+[ -z "$GRAPH_PATH" ] && GRAPH_PATH=/var/www/f00.fr/tesla/graph_${PERIOD}.png
 
-rrdtool graph ${GRAPH_PATH} --start -1${PERIOD} -t "Energy usage of the last ${PERIOD}" -z \
--c BACK#FFFFFF \
--c SHADEA#000000 \
--c SHADEB#FFFFFF \
--c MGRID#AAAAAA \
--c GRID#CCCCCC \
--c ARROW#333333 \
--c FONT#333333 \
--c AXIS#333333 \
--c FRAME#333333 \
--h 480 -w 1024 -l 0 -a PNG -v W \
-DEF:energy=${DB_PATH}:energy:AVERAGE:step=60 \
-VDEF:min=energy,MINIMUM \
-VDEF:max=energy,MAXIMUM \
-VDEF:avg=energy,AVERAGE \
-VDEF:lst=energy,LAST \
-CDEF:smooth_energy=energy,300,TREND \
-COMMENT:"\n" \
-COMMENT:"\tMinimum" \
-COMMENT:"\tMaximum" \
-COMMENT:"\tAverage" \
-COMMENT:"\tCurrent" \
-COMMENT:"\n" \
-GPRINT:min:"\t%5.1lf%sW" \
-GPRINT:max:"\t%5.1lf%sW" \
-GPRINT:avg:"\t%5.1lf%sW" \
-GPRINT:lst:"\t%5.1lf%sW\n" \
-AREA:energy#EDA362:Usage  \
-LINE1:energy#F47200
-
+rrdtool graph ${GRAPH_PATH} --start -1${PERIOD} \
+	-t "Energy usage of the last ${PERIOD}" \
+	-z \
+	--slope-mode \
+	--font DEFAULT:7: \
+	--watermark "$(date)" \
+	--vertical-label "energy(watt)" \
+	--lower-limit 0 \
+	--right-axis 1:0 \
+	--alt-y-grid --rigid \
+	-h 480 -w 1024 -l 0 -a PNG \
+	DEF:energy=${DB_PATH}:energy:AVERAGE:step=60 \
+	VDEF:min=energy,MINIMUM \
+	VDEF:max=energy,MAXIMUM \
+	VDEF:avg=energy,AVERAGE \
+	VDEF:lst=energy,LAST \
+	LINE1:energy#000000:"energy (watt)" \
+	COMMENT:"\n" \
+	COMMENT:"\t\t\t\t\t\t\tMinimum" \
+	COMMENT:"\tMaximum" \
+	COMMENT:"\tAverage" \
+	COMMENT:"\tCurrent" \
+	COMMENT:"\n" \
+	GPRINT:min:"\t\t\t\t\t\t\t%5.1lf%sW" \
+	GPRINT:max:"\t%5.1lf%sW" \
+	GPRINT:avg:"\t%5.1lf%sW" \
+	GPRINT:lst:"\t%5.1lf%sW\n"
